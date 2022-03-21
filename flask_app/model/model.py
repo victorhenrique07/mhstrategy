295535b9a70db407cd56413e15729ae5159047e8
Victor Henrique Silva <henrictor@hotmail.com>
2022-03-21 19:44:07 -0300

    Added database models and database config

---
+++ flask_app/model/model.py
@@ -0,0 +1,33 @@
+from flask_app.config.db import db
+
+
+class Monster(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=False, nullable=False)
+    weakness = db.Column(db.String(255), unique=False, nullable=False)
+    strong = db.Column(db.String(255), unique=False, nullable=False)
+    build = db.Column(db.Text, unique=False, nullable=False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "weakness": self.weakness,
+            "strong": self.strong,
+            "build": self.build,
+        }
+
+
+class User():
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(30), unique=False, nullable=False)
+    username = db.Column(db.String(45), unique=True, nullable=False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "password": self.password,
+            "username": self.username
+        }
