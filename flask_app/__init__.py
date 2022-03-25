c763279ca9879978b5a9f8dcbf17e54231d3f8aa
Victor Henrique Silva <henrictor@hotmail.com>
2022-03-25 15:36:21 -0300

    changed database URL

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,7 +4,7 @@ from os import path
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mariadb+pymysql://rick:12345@localhost/MHStrategy"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://wlmxxjvkbxzalz:466a1ee4eed0338d495cfe18a52a04e54a794e35ff6bc2324e464e0c006efb29@ec2-3-225-213-67.compute-1.amazonaws.com:5432/dfjcq3qk0jsdk3"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     
