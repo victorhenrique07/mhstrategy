d138bc094df8e4bc9b279e8ab6caabf419aaf407
Victor Henrique Silva <henrictor@hotmail.com>
2022-03-25 13:25:09 -0300

    added with app.app_context()

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
-from db import db
+from .db import db
 from os import path
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = "mariadb+pymysql://rick:12345@localhost/MHStrategy"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
+    
     
     from .controller.controller import auth
     
     app.register_blueprint(auth, url_prefix='/')
+    
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
     
     return app
