0796b7cb919c8cea20d8ed5348d7bf5f56e41dc0
Victor Henrique Silva <henrictor@hotmail.com>
2022-03-25 13:23:38 -0300

    using create_app() to initialize flask_app

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
-from flask_app.controller.controller import create_routes
-from flask import Flask
-import os
-from flask_app.db import db
+from flask_app import create_app
 
-DATABASE_URI = os.getenv('DATABASE_URI')
+app = create_app()
 
-app = Flask(__name__)
-create_routes(app)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True)
