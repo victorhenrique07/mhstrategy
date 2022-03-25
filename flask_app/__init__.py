b0c85779be846dec14bc02153b95f75a3915b1f8
Victor Henrique Silva <henrictor@hotmail.com>
2022-03-24 22:37:19 -0300

    created create_app() function on __init__.py inside flask_app

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,7 +4,7 @@ from os import path
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mariadb+pymysql://root:12345@localhost/MHStrategy"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mariadb+pymysql://rick:12345@localhost/MHStrategy"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     
