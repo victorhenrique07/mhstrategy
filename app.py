02790c5856a1d9055bcbe5dfd1092b372131e07d
Victor Henrique Silva <henrictor@hotmail.com>
2022-04-14 17:39:11 -0300

    deleted controller directory

--- app.py
+++ app.py
@@ -2,11 +2,5 @@ from flask_app import create_app
 
 app = create_app()
 
-<<<<<<< HEAD
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-=======
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> rotascadastro
